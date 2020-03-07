5a9572b8928465b1ad292de0b9f51885dbf166ca
xsl <x976403038@163.com>
2020-03-07 21:25:00 +0800

    @后面没加/导致的错误

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入模块
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-import '@styles/index.less' // 引入样式
+import '@/styles/index.less' // 引入样式
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册Element-ui
 new Vue({
