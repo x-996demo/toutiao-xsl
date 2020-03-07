1f229cab4610e3488092c73cfababa440e8aa450
xsl <x976403038@163.com>
2020-03-07 20:39:37 +0800

    项目初始化安装elementui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 引入模块
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册Element-ui
 new Vue({
   router,
   render: h => h(App)
