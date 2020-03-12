ce8b52e80ba8de11123a9f5b750a930265663fdc
xsl <x976403038@163.com>
2020-03-12 22:02:47 +0800

    获取用户信息

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -10,10 +10,10 @@
     <el-col :span="12" class="right">
       <!-- 再次放入row组件 align属性垂直居中 -->
       <el-row type="flex" justify="end" align="middle">
-        <img src="http://b-ssl.duitang.com/uploads/item/201509/22/20150922180033_hV43u.jpeg" alt />
+        <img :src="userInfo.photo" alt="" />
         <!-- 下拉菜单 -->
         <el-dropdown trigger="click">
-          <span>软萌嘤嘤嘤</span>
+          <span>{{userInfo.name}}</span>
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item>个人信息</el-dropdown-item>
             <el-dropdown-item>github地址</el-dropdown-item>
@@ -26,7 +26,27 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      userInfo: {} // 用户个人信息
+    }
+  },
+  created () {
+    const token = localStorage.getItem('user-token') // 从兜里拿token 也就是从缓存中取token
+    // 获取用户个人信息
+    this.$axios({
+      url: '/user/profile', // 请求地址
+      headers: {
+        Authorization: `Bearer ${token}` // 格式要求 Bearer+token
+
+      }// 请求头参数 headers放置请求头参数
+    }).then(result => {
+      // 若果加载成功了 我们要将数据赋值给 userinfo
+      this.userInfo = result.data.data
+    })
+  }
+}
 </script>
 
 <style lang='less' scoped>
