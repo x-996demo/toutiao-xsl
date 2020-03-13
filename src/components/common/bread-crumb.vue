44c4ceeec13bd3b90341b3701f3c30060aca7d02
xsl <x976403038@163.com>
2020-03-13 19:32:19 +0800

    封装面包屑组件

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,22 @@
+<template>
+<!-- 使用elementui组件 封装面包屑组件 设置分隔符-->
+  <el-breadcrumb separator=">">
+      <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+      <!-- 第二个选项是动态的 需要采用传值的方式传进来 props传值 slot插槽(采用) -->
+      <el-breadcrumb>
+          <!-- 内容是动态的 -->
+          <!-- 定义一个具名插槽 -->
+          <slot class="title"></slot>
+      </el-breadcrumb>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
